43fde2c563d39b3e9453cadbd65ec5508da68deb
quytb.hn <quytb.hn@havaz.vn>
2020-02-15 12:08:05 +0700

    initial project

---
+++ src/main/java/com/havaz/config/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.havaz.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
